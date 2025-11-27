4c87b465fef9b6b2137c9e30582da556c125153b
LeggyBoiTim <105779117+LeggyBoiTim@users.noreply.github.com>
2025-11-27 15:02:33 +0100

    DiceThrower

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+import ThrownDice from './components/ThrownDice.vue';
+import ScoreTable from './components/ScoreTable.vue';
+</script>
+
+<template>
+  <div id="app">
+    <ThrownDice />
+    <ScoreTable />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/ThrownDice.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from 'vue';
+
+const diceArray = ref([]);
+const diceCount = ref({
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0
+});
+
+const throwDice = () => {
+    diceArray.value = [];
+    diceCount.value = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0};
+    for (let i = 0; i < 5; i++) {
+        const result = Math.floor(Math.random() * 6) + 1;
+        diceArray.value.push(result);
+        diceCount.value[result]++;
+    }
+    console.log(diceArray.value, diceCount.value);
+}
+</script>
+
+<template>
+    <h1>Scoreblock</h1>
+    <button @click="throwDice">Throw!</button>
+    <table>
+        <tr>
+            <td v-for="(amount, index) in diceArray" :key="index">
+                <p>{{ amount }}</p>
+            </td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+</style>
